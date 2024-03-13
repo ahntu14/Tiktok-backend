6937000a41d1f9aa8ce20e91a31bc40a36f76dc1
phamtu14 <20020493@vnu.edu.vn>
2024-03-13 23:19:30 +0700

    first

---
+++ src/config/environment.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+export const env = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  APP_PORT: process.env.PORT,
+  JWT_ACCESS_KEY: process.env.JWT_ACCESS_KEY,
+  JWT_REFRESH_KEY: process.env.JWT_REFRESH_KEY,
+  MAIL_USERNAME: process.env.MAIL_USERNAME,
+  MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+  HOST_NAME: process.env.HOST_NAME,
+};
---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import mongoose from 'mongoose'
+import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware.js";
+import { env } from "./config/environment.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(errorHandlingMiddleware);
+
+mongoose
+  .connect(env.MONGODB_URI)
+  .then(() => {
+    app.listen(env.APP_PORT, () => {
+      console.log(
+        `Server is running on http://${env.HOST_NAME}:${env.APP_PORT}`
+      );
+    });
+
+    console.log("Connected to MongoDb");
+  })
+  .catch((error) => console.log(error));
